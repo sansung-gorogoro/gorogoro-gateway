cb87de77c59eed5a662bbe50f05d374e860e3282
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-12-24 16:57:20 +0900

    feat(common): JWT 토큰을 검증하는 GatewayFilter 생성

--- src/main/java/com/gorogoro/gateway/exception/code/GatewayErrorCode.java
+++ src/main/java/com/gorogoro/gateway/exception/code/GatewayErrorCode.java
@@ -9,11 +9,12 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GatewayErrorCode implements ErrorCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "GTW-0001", "유효하지 않은 토큰입니다."),
-    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "GTW-0002", "접근 권한이 없습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GTW-0003", "서버 내부 오류가 발생했습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "GTW-0004", "잘못된 요청입니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND, "GTW-0005", "존재하지 않는 경로입니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "GTW-0006", "허용되지 않은 HTTP 메서드입니다.");
+    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "GTW-0002", "유효하지 않은 토큰입니다."),
+    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "GTW-0003", "접근 권한이 없습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GTW-0004", "서버 내부 오류가 발생했습니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "GTW-0005", "잘못된 요청입니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "GTW-0006", "존재하지 않는 경로입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "GTW-0007", "허용되지 않은 HTTP 메서드입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
---
+++ src/main/java/com/gorogoro/gateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,122 @@
+package com.gorogoro.gateway.filter;
+
+import com.gorogoro.gateway.exception.BaseException;
+import com.gorogoro.gateway.exception.code.GatewayErrorCode;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import jakarta.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    private static final String ALGORITHM = "RSA";
+    private static final String ROLE_KEY = "role";
+    private static final String UNKNOWN_ROLE = "UNKNOWN";
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String HEADER_USER_ID = "X-User-Id";
+    private static final String HEADER_USER_ROLE = "X-User-Role";
+    private static final String KEY_HEADER = "-----BEGIN PUBLIC KEY-----";
+    private static final String KEY_FOOTER = "-----END PUBLIC KEY-----";
+
+    private final Resource publicKeyResource;
+    private PublicKey publicKey;
+
+    public AuthorizationHeaderFilter(@Value("classpath:public_key.pem") Resource publicKeyResource) {
+        super(Config.class);
+        this.publicKeyResource = publicKeyResource;
+    }
+
+    public static class Config {
+    }
+
+    @PostConstruct
+    private void loadPublicKey() {
+        try {
+            // 파일 읽기
+            String publicKeyString = StreamUtils.copyToString(publicKeyResource.getInputStream(), StandardCharsets.UTF_8);
+
+            // 헤더, 푸터, 공백 제거
+            String publicKey = publicKeyString
+                    .replace(KEY_HEADER, "")
+                    .replace(KEY_FOOTER, "")
+                    .replaceAll("\\s", "");
+
+            // Base64 디코딩 및 PublicKey 객체 생성
+            byte[] encoded = Base64.getDecoder().decode(publicKey);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+            this.publicKey = KeyFactory.getInstance(ALGORITHM).generatePublic(keySpec);
+
+            System.out.println("공개키 로딩 완료");
+        } catch (Exception e) {
+            throw new RuntimeException("공개키를 로드하는 데 실패했습니다.", e);
+        }
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            // 요청 정보 추출
+            ServerHttpRequest serverHttpRequest = exchange.getRequest();
+
+            String authHeader = serverHttpRequest.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null) {
+                throw new BaseException(GatewayErrorCode.EMPTY_TOKEN);
+            }
+
+            if (!authHeader.startsWith(BEARER_PREFIX)) {
+                throw new BaseException(GatewayErrorCode.INVALID_TOKEN);
+            }
+
+            // 토큰 추출
+            String token = authHeader.substring(BEARER_PREFIX.length());
+
+            try {
+                // 공개 키를 사용하여 토큰이 변조 여부 검증 및 파싱
+                Claims claims = Jwts.parser()
+                        .verifyWith(getPublicKey())
+                        .build()
+                        .parseSignedClaims(token)
+                        .getPayload();
+
+                String userId = claims.getSubject();
+                String role = claims.get(ROLE_KEY, String.class) != null
+                        ? claims.get(ROLE_KEY, String.class)
+                        : UNKNOWN_ROLE;
+
+                ServerHttpRequest newRequest = serverHttpRequest.mutate()
+                        .headers(header -> {
+                            header.remove(HttpHeaders.AUTHORIZATION);
+                            header.add(HEADER_USER_ID, userId);
+                            header.add(HEADER_USER_ROLE, role);
+                        })
+                        .build();
+
+                return chain.filter(exchange.mutate().request(newRequest).build());
+            } catch (Exception e) {
+                throw new BaseException(GatewayErrorCode.UNAUTHORIZED_ACCESS);
+            }
+        };
+    }
+
+    private PublicKey getPublicKey() {
+        if (this.publicKey == null) {
+            throw new RuntimeException("공개키가 초기화되지 않았습니다.");
+        }
+
+        return this.publicKey;
+    }
+}
